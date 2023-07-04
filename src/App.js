be4cbc73cebddd91ce2930f802787d94526286dd
proprakhar <63116642+proprakhar@users.noreply.github.com>
2023-07-04 23:58:45 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@material-ui/core/styles";
+import React from "react";
+import theme from "./theme";
+import { CssBaseline } from "@mui/material";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div></div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
